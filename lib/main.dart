666f7f945deac279bf85735eebc2c2eba1d37327
Vlad-Skv <vlad-skv@macmini.local>
2021-02-16 16:20:43 +0300

    new sight_list screen, removed test widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:places/ui/screen/sight_list_screen.dart';
 
 void main() {
   runApp(App());
@@ -8,54 +9,8 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyFirstWidget(),
+      home: SightListScreen(),
       title: 'Some title',
     );
-  }
-}
-
-class MyFirstWidget extends StatelessWidget {
-  var counter = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    counter++;
-    print('counter: $counter');
-
-    return Container(
-      child: Center(
-        child: Text('Hello!'),
-      ),
-    );
-  }
-
-  getContextType() {
-    // Undefined name: context
-    //return context.runtimeType;
-  }
-}
-
-class MyFirstStatefulWidget extends StatefulWidget {
-  @override
-  _MyFirstStatefulWidgetState createState() => _MyFirstStatefulWidgetState();
-}
-
-class _MyFirstStatefulWidgetState extends State<MyFirstStatefulWidget> {
-  var counter = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    counter++;
-    print('counter: $counter');
-
-    return Container(
-      child: Center(
-        child: Text('Hello!'),
-      ),
-    );
-  }
-
-  getContextType() {
-    return context.runtimeType;
   }
 }
