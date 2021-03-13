93258b1f387a18b060b34f2a703ab50aac098eaa
Vlad-Skv <vlad-skv@macmini.local>
2021-03-13 23:48:20 +0300

    added appbar text by design

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:places/constants.dart';
 
 class SightListScreen extends StatefulWidget {
   @override
@@ -8,9 +9,23 @@ class SightListScreen extends StatefulWidget {
 class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
+    var bgColor = Theme.of(context).scaffoldBackgroundColor;
     return Scaffold(
       appBar: AppBar(
-        title: Text("Sight List"),
+        title: Text(
+          "Список\nинтересных мест",
+          style: TextStyle(
+            color: kAppBarTextColor,
+            fontSize: 32,
+            fontFamily: 'Roboto',
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        backgroundColor: bgColor,
+        brightness: ThemeData.estimateBrightnessForColor(bgColor),
+        elevation: 0,
+        centerTitle: false,
+        toolbarHeight: 72.0,
       ),
     );
   }
